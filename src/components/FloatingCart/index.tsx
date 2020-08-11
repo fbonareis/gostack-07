f68588b7ad270cb364e4506bb9191d777cbbf8ec
Fernando de Bona dos Reis <fbonareis@gmail.com>
2020-08-11 15:18:52 -0300

    resolved tests

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,16 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const subtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + subtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
-  const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
-  }, [products]);
+  const totalItensInCart = useMemo(
+    () => products.reduce((sum, { quantity }) => sum + quantity, 0),
+    [products],
+  );
 
   return (
     <Container>
